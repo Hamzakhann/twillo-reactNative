9cfd776c42e21f06ae9079e2395136cf2af927af
Hamzakhann <hamzakhann66@gmail.com>
2018-09-25 10:32:10 +0500

    add cards with image and button

---
+++ src/Deck/Deck.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {View , Animated} from 'react-native';
+class Deck extends Component {
+
+  renderCards = () =>{
+    return this.props.data.map(item =>{
+        return this.props.renderCards(item);
+    })
+  }
+  render() {
+    return (
+      <View>
+       {this.renderCards} 
+      </View>
+    )
+  }
+}
+
+export default Deck;
